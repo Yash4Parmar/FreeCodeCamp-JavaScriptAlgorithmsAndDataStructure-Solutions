583a55a9ae1d520a0e257959db3cacd827cd4ae5
--global <--global>
2022-03-12 17:44:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,99 @@
+//console.log("hello!!!");
+// let a = [1,2,3,4,5];
+//console.log(a);
+/*class object {
+    constructor(name1,height1,weigth1,color1){
+        this.name = name1;
+        this.hight =  height1,
+        this.weight =  weigth1,
+        this.color =  color1
+    }
+     get Color(){
+         this.name = name1;
+     }
+
+}
+
+dog = new object('abcd',55,34,'black');
+dog.name = 'asfi';
+console.log(dog.name);*/
+
+
+
+/*let dog = {
+    name: 'abcd',
+    hight: 80,
+    weight: 50,
+    color: 'black'
+}
+console.log(dog);
+console.log(dog.name);*/
+
+/*let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//  arr.splice(1,5,2222, 3333, 4444, 5555);
+let arr2 = arr.slice(0, 3);
+console.log(arr2);
+console.log(arr.length);
+
+let weatherConditions = ['rain', 'snow', 'sleet', ...arr, 'hail', 'clear'];
+
+// let todaysWeather = weatherConditions.slice(1, 4);
+// console.log(todaysWeather);
+console.log(weatherConditions);*/
+
+/*let a = [1,2,3,4,5,6];
+a.push(7);
+a.unshift(0);
+console.log(a);*/
+/*let obj = {
+    name: "abcd",
+    num:3434,
+    height: 67,
+    weight: 12
+};
+obj.color = "black";
+let a = Object.keys(obj);
+console.log(a);*/
+
+/*let a = 'ABCD';
+const b = a.slice('');
+const c = [];
+for(let i = b.length - 1; i >=0 ; i--){
+    c.push(b[i]);
+}
+console.log(c);*/
+//***************************************************************************************************** */
+/*function reverseString(str) {
+   const a =  str;
+   let resstr = '';
+   const b = a.slice('');
+   const c = [];
+
+
+   for(let i = b.length - 1; i >=0 ; i--){
+    // c.push(b[i]);
+    resstr += b[i];
+}
+// console.log(str);
+// console.log(c);
+    // str = c.toString();
+//  console.log(resstr);
+    return resstr;
+    
+  }
+  //   reverseString("hello");
+  console.log(reverseString("hello"));*/
+  
+  //   let one = ["hi"," my name is yash ",222]
+  //   let three = one[0] + one[1] +  one[2];
+  //   console.log(three);
+  /*function reverseString(str) {
+      console.log(str[0]);
+      let reversedStr = "";
+      for (let i = str.length - 1; i >= 0; i--) {
+          reversedStr += str[i];
+        }
+        return reversedStr;
+    }*/
+/***************************************************************************************************************** */
+   
---
+++ new.js
@@ -0,0 +1,52 @@
+/*function factorialize(num) {
+    let sum = 1;
+    // console.log(num);
+    if(num > 0){
+
+        for(let i = num; i > 0; i--){
+            sum = sum * i;
+        }
+    }
+    num = sum;
+    console.log(num);
+    return num;
+  }
+  
+  factorialize(5);*/
+
+ /* function findLongestWordLength(str) {
+    const a = str;
+    const b = a.split(' ');
+    // console.log(str);
+    // console.log(b[0].length);
+    var mx = 1;
+    for(let i = 0; i < b.length; i++){
+        if(mx < b[i].length){
+            mx = b[i].length;
+        }
+    }
+    // console.log(mx);
+    return mx;
+  }
+  console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+//   findLongestWordLength("The quick brown fox jumped over the lazy dog");*/
+
+function largestOfFour(arr) {
+   
+    let l;
+    let result = [];
+
+    for(let i = 0; i < arr.length; i++){
+        l = arr[i][0];
+        for(let j = 0; j < arr[i].length; j++){
+            if(arr[i][j] > l){
+                l = arr[i][j];
+            }
+        }
+        result[i] = l;
+    }
+     console.log(result);
+    return result;
+  }
+  
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
